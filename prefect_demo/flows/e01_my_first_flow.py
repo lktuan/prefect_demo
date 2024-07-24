b29936739f77e4508b8955d60da9a900624dce8a
Tuan Le Khac <tuan.lekhac0905@gmail.com>
2024-07-24 17:33:11 +0700

    stupid commit, should not work on master

--- prefect_demo/flows/e01_my_first_flow.py
+++ prefect_demo/flows/e01_my_first_flow.py
@@ -3,21 +3,20 @@ from prefect import flow, task
 
 
 @task(name="Addition operator")  # this task will successfully run
-def add(a, b):
+def add(a: int = 0, b: int = 0) -> int:
     return a + b
 
 
 @task(name="Squaring operator")  # this task will fail
-def square_num(num):
-    if True:
-        raise ValueError
-    return num ** 2
+def square_num(num: int = 0) -> int:
+    # if True:
+    #     raise ValueError
+    return num**2
 
 
-@flow(log_prints=True,
-      name="Add and Square",
-      description="My first simple flow")
+@flow(log_prints=True, name="Add and Square", description="My first simple flow")
 def add_and_square(a: int = 2, b: int = 3):
     add_result = add(a, b)
     square_result = square_num(add_result)
     print(f"({a} + {b}) squared = {square_result}")
+    return square_result
--- tests/test_flow_e01.py
+++ tests/test_flow_e01.py
@@ -1,7 +1,5 @@
-# content of test_sample.py
-def inc(x):
-    return x + 1
+from prefect_demo.flows.e01_my_first_flow import add_and_square
 
 
-def test_answer():
-    assert inc(3) == 5
+def test_add_and_square():
+    assert add_and_square(4, 8) == 144, "function add_and_square() not working properly"
